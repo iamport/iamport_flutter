8909d507e279a0e847420e402b04e74dff245538
Hyukgyu Kwon <me@kwon.hk>
2021-05-26 17:55:37 +0900

    각 플랫폼별 기본 언어 변경 및 빌드 문제 해결
    - 안드로이드 kotlin 추가
    - iOS 스위프트 추가

--- example/android/app/src/main/java/kr/iamport/iamport_flutter_example/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package kr.iamport.iamport_flutter_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- ios/Classes/IamportFlutterPlugin.m
+++ ios/Classes/IamportFlutterPlugin.m
@@ -1,20 +1,17 @@
 #import "IamportFlutterPlugin.h"
 
+#if __has_include(<iamport_flutter/iamport_flutter-Swift.h>)
+#import <iamport_flutter/iamport_flutter-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "flutter_plugin_example-Swift.h"
+#import "iamport_flutter-Swift.h"
+#endif
+
 @implementation IamportFlutterPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"iamport_flutter"
-            binaryMessenger:[registrar messenger]];
-  IamportFlutterPlugin* instance = [[IamportFlutterPlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+  [SwiftIamportFlutterPlugin registerWithRegistrar:registrar];
 }
-
-- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
-}
-
 @end
