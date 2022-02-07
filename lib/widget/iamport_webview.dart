7b42b0bf0a83f1525ec5727abe7d4ff0992ab1cf
SuhwanCha <suhw4n@gmail.com>
2022-02-07 16:08:53 +0900

    sync from upstream

--- lib/widget/iamport_webview.dart
+++ lib/widget/iamport_webview.dart
@@ -13,7 +13,7 @@ class IamportWebView extends StatefulWidget {
     <html>
       <head>
         <meta http-equiv="content-type" content="text/html; charset=utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 
         <script type="text/javascript" src="https://code.jquery.com/jquery-latest.min.js" ></script>
         <script type="text/javascript" src="https://cdn.iamport.kr/js/iamport.payment-1.2.0.js"></script>
