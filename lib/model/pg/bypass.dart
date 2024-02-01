137763634510d1defd8e0d961f8abd3745b297d5
Hyukgyu Kwon <me@kwon.hk>
2024-02-01 17:55:25 +0900

    feat: 현금영수증 노출 제어 파라미터들 추가

--- lib/model/pg/bypass.dart
+++ lib/model/pg/bypass.dart
@@ -1,3 +1,4 @@
+import 'package:iamport_flutter/model/pg/danal/danal.dart';
 import 'package:iamport_flutter/model/pg/daou/daou.dart';
 import 'package:iamport_flutter/model/pg/tosspayments/tosspayments.dart';
 import 'package:iamport_flutter/model/pg/settle/settle.dart';
@@ -9,16 +10,27 @@ part 'bypass.g.dart';
 class Bypass {
   bool? isCulturalExpense;
   String? cashReceiptType;
+
+  // https://guide.portone.io/aad7f7a4-366b-46fc-8e59-f1d79c986b3e
+  @JsonKey(name: "acceptmethod")
+  String? acceptMethod;
+  @JsonKey(name: "P_RESERVED")
+  String? pReserved;
+
   Daou? daou;
   Tosspayments? tosspayments;
   Settle? settle;
+  Danal? danal;
 
   Bypass({
     this.isCulturalExpense,
     this.cashReceiptType,
+    this.acceptMethod,
+    this.pReserved,
     this.daou,
     this.tosspayments,
     this.settle,
+    this.danal,
   });
 
   factory Bypass.fromJson(Map<String, dynamic> json) => _$BypassFromJson(json);
