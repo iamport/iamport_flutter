a685258d0f6c790af4b401f648f95dbfd209c2d0
Dongmin Yu <ydm2790@gmail.com>
2024-05-25 01:48:56 +0900

    refactor: 🎨 안드로이드 코틀린 코드 포맷

--- android/src/main/kotlin/kr/iamport/iamport_flutter/IamportFlutterPlugin.kt
+++ android/src/main/kotlin/kr/iamport/iamport_flutter/IamportFlutterPlugin.kt
@@ -1,7 +1,6 @@
 package kr.iamport.iamport_flutter
 
 import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -9,27 +8,36 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** IamportFlutterPlugin */
-class IamportFlutterPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class IamportFlutterPlugin : FlutterPlugin, MethodCallHandler {
+    /**
+     * The MethodChannel that will the communication between Flutter and native Android
+     *
+     * This local reference serves to register the plugin with the Flutter Engine and unregister it
+     * when the Flutter Engine is detached from the Activity
+     */
+    private lateinit var channel: MethodChannel
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "iamport_flutter")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    override fun onAttachedToEngine(
+        @NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding,
+    ) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "iamport_flutter")
+        channel.setMethodCallHandler(this)
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onMethodCall(
+        @NonNull call: MethodCall,
+        @NonNull result: Result,
+    ) {
+        if (call.method == "getPlatformVersion") {
+            result.success("Android ${android.os.Build.VERSION.RELEASE}")
+        } else {
+            result.notImplemented()
+        }
+    }
+
+    override fun onDetachedFromEngine(
+        @NonNull binding: FlutterPlugin.FlutterPluginBinding,
+    ) {
+        channel.setMethodCallHandler(null)
+    }
 }
