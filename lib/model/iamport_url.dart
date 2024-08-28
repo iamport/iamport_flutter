ccfe84dfa4103cf4018c69ee66d03d0e38c65525
Dongmin Yu <ydm2790@gmail.com>
2024-08-28 13:39:46 +0900

    fix: 🎨 프라이빗 변수 프리픽스 언더바 삽입

--- lib/model/iamport_url.dart
+++ lib/model/iamport_url.dart
@@ -1,21 +1,16 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:iamport_flutter/model/url_data.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
+import 'package:iamport_flutter/model/url_data.dart';
+
 class IamportUrl {
-  late String url;
-  String? appScheme;
-  String? appUrl;
-  String? package; // Android only
-
   IamportUrl(String incomeUrl) {
-    this.url = incomeUrl;
+    _url = incomeUrl;
 
-    List<String> splittedUrl =
-        this.url.replaceFirst(RegExp(r'://'), ' ').split(' ');
-    this.appScheme = splittedUrl[0];
+    final splittedUrl = _url.replaceFirst(RegExp('://'), ' ').split(' ');
+    _appScheme = splittedUrl[0];
 
     if (Platform.isAndroid) {
       /*
@@ -25,55 +20,58 @@ class IamportUrl {
         3. intent:[app]://
         이 세가지를 정상적으로 launch가 가능한 2번 형태로 변환한다
       */
-      if (this.isAppLink()) {
-        if (this.appScheme!.contains('intent')) {
-          List<String> intentUrl = splittedUrl[1].split('#Intent;');
-          String host = intentUrl[0];
+      if (isAppLink()) {
+        if (_appScheme!.contains('intent')) {
+          final intentUrl = splittedUrl[1].split('#Intent;');
+          var host = intentUrl[0];
           // 농협카드 일반결제 예외처리
           if (host.contains(':')) {
-            host = host.replaceAll(RegExp(r':'), '%3A');
+            host = host.replaceAll(RegExp(':'), '%3A');
           }
-          List<String> arguments = intentUrl[1].split(';');
+          final arguments = intentUrl[1].split(';');
 
           // scheme이 intent로 시작하면 뒷쪽의 정보를 통해 appscheme과 package 정보 추출
-          if (this.appScheme! != 'intent') {
+          if (_appScheme! != 'intent') {
             // 현대카드 예외처리
-            this.appScheme = this.appScheme!.split(':')[1];
-            this.appUrl = this.appScheme! + '://' + host;
+            _appScheme = _appScheme!.split(':')[1];
+            _appUrl = '${_appScheme!}://$host';
           }
-          arguments.forEach((s) {
+          for (final s in arguments) {
             if (s.startsWith('scheme')) {
-              String scheme = s.split('=')[1];
-              this.appUrl = scheme + '://' + host;
-              this.appScheme = scheme;
+              final scheme = s.split('=')[1];
+              _appUrl = '$scheme://$host';
+              _appScheme = scheme;
             } else if (s.startsWith('package')) {
-              String package = s.split('=')[1];
-              this.package = package;
+              final package = s.split('=')[1];
+              _package = package;
             }
-          });
+          }
         } else {
-          this.appUrl = this.url;
+          _appUrl = _url;
         }
       } else {
-        this.appUrl = this.url;
+        _appUrl = _url;
       }
     } else {
-      this.appUrl = this.url;
+      _appUrl = _url;
     }
   }
+
+  late String _url;
+  String? _appScheme;
+  String? _appUrl;
+  String? _package; // Android only
 
   bool isAppLink() {
     String? scheme;
     try {
-      scheme = Uri.parse(this.url).scheme;
+      scheme = Uri.parse(_url).scheme;
     } catch (e) {
-      scheme = this.appScheme;
+      scheme = _appScheme;
     }
 
-    if (Platform.isAndroid && this.appScheme == 'https') {
-      if (this
-          .url
-          .startsWith('https://play.google.com/store/apps/details?id=')) {
+    if (Platform.isAndroid && _appScheme == 'https') {
+      if (_url.startsWith('https://play.google.com/store/apps/details?id=')) {
         return true;
       }
     }
@@ -82,75 +80,75 @@ class IamportUrl {
   }
 
   Future<String?> getAppUrl() async {
-    return this.appUrl;
+    return _appUrl;
   }
 
   Future<String?> getMarketUrl() async {
     if (Platform.isIOS) {
-      switch (this.appScheme) {
+      switch (_appScheme) {
         case 'kftc-bankpay': // 뱅크페이
-          return UrlData.IOS_MARKET_PREFIX + 'id398456030';
+          return '${UrlData.IOS_MARKET_PREFIX}id398456030';
         case 'ispmobile': // ISP/페이북
-          return UrlData.IOS_MARKET_PREFIX + 'id369125087';
+          return '${UrlData.IOS_MARKET_PREFIX}id369125087';
         case 'hdcardappcardansimclick': // 현대카드 앱카드
-          return UrlData.IOS_MARKET_PREFIX + 'id702653088';
+          return '${UrlData.IOS_MARKET_PREFIX}id702653088';
         case 'shinhan-sr-ansimclick': // 신한 앱카드
-          return UrlData.IOS_MARKET_PREFIX + 'id572462317';
+          return '${UrlData.IOS_MARKET_PREFIX}id572462317';
         case 'kb-acp': // KB국민 앱카드
-          return UrlData.IOS_MARKET_PREFIX + 'id695436326';
+          return '${UrlData.IOS_MARKET_PREFIX}id695436326';
         case 'mpocket.online.ansimclick': // 삼성앱카드
-          return UrlData.IOS_MARKET_PREFIX + 'id535125356';
+          return '${UrlData.IOS_MARKET_PREFIX}id535125356';
         case 'lottesmartpay': // 롯데 모바일결제
-          return UrlData.IOS_MARKET_PREFIX + 'id668497947';
+          return '${UrlData.IOS_MARKET_PREFIX}id668497947';
         case 'lotteappcard': // 롯데 앱카드
-          return UrlData.IOS_MARKET_PREFIX + 'id688047200';
+          return '${UrlData.IOS_MARKET_PREFIX}id688047200';
         case 'cloudpay': // 하나1Q페이(앱카드)
-          return UrlData.IOS_MARKET_PREFIX + 'id847268987';
+          return '${UrlData.IOS_MARKET_PREFIX}id847268987';
         case 'citimobileapp': // 시티은행 앱카드
-          return UrlData.IOS_MARKET_PREFIX + 'id1179759666';
+          return '${UrlData.IOS_MARKET_PREFIX}id1179759666';
         case 'payco': // 페이코
-          return UrlData.IOS_MARKET_PREFIX + 'id924292102';
+          return '${UrlData.IOS_MARKET_PREFIX}id924292102';
         case 'kakaotalk': // 카카오톡
-          return UrlData.IOS_MARKET_PREFIX + 'id362057947';
+          return '${UrlData.IOS_MARKET_PREFIX}id362057947';
         case 'lpayapp': // 롯데 L.pay
-          return UrlData.IOS_MARKET_PREFIX + 'id1036098908';
+          return '${UrlData.IOS_MARKET_PREFIX}id1036098908';
         case 'wooripay': // 우리페이
-          return UrlData.IOS_MARKET_PREFIX + 'id1201113419';
+          return '${UrlData.IOS_MARKET_PREFIX}id1201113419';
         case 'com.wooricard.wcard': // 우리WON카드
-          return UrlData.IOS_MARKET_PREFIX + 'id1499598869';
+          return '${UrlData.IOS_MARKET_PREFIX}id1499598869';
         case 'nhallonepayansimclick': // NH농협카드 올원페이(앱카드)
-          return UrlData.IOS_MARKET_PREFIX + 'id1177889176';
+          return '${UrlData.IOS_MARKET_PREFIX}id1177889176';
         case 'hanawalletmembers': // 하나카드(하나멤버스 월렛)
-          return UrlData.IOS_MARKET_PREFIX + 'id1038288833';
+          return '${UrlData.IOS_MARKET_PREFIX}id1038288833';
         case 'shinsegaeeasypayment': // 신세계 SSGPAY
-          return UrlData.IOS_MARKET_PREFIX + 'id666237916';
+          return '${UrlData.IOS_MARKET_PREFIX}id666237916';
         case 'naversearchthirdlogin': // 네이버페이 앱 로그인
-          return UrlData.IOS_MARKET_PREFIX + 'id393499958';
+          return '${UrlData.IOS_MARKET_PREFIX}id393499958';
         case 'lguthepay-xpay': // 페이나우
-          return UrlData.IOS_MARKET_PREFIX + 'id760098906';
+          return '${UrlData.IOS_MARKET_PREFIX}id760098906';
         case 'lmslpay': // 롯데 L.POINT
-          return UrlData.IOS_MARKET_PREFIX + 'id473250588';
+          return '${UrlData.IOS_MARKET_PREFIX}id473250588';
         case 'liivbank': // Liiv 국민
-          return UrlData.IOS_MARKET_PREFIX + 'id1126232922';
+          return '${UrlData.IOS_MARKET_PREFIX}id1126232922';
         case 'supertoss': // 토스
-          return UrlData.IOS_MARKET_PREFIX + 'id839333328';
+          return '${UrlData.IOS_MARKET_PREFIX}id839333328';
         case 'newsmartpib': // 우리WON뱅킹
-          return UrlData.IOS_MARKET_PREFIX + 'id1470181651';
+          return '${UrlData.IOS_MARKET_PREFIX}id1470181651';
         case 'v3mobileplusweb': // V3 Mobile Plus
-          return UrlData.IOS_MARKET_PREFIX + 'id1481938658';
+          return '${UrlData.IOS_MARKET_PREFIX}id1481938658';
         case 'kbbank': // KB스타뱅킹
-          return UrlData.IOS_MARKET_PREFIX + 'id373742138';
+          return '${UrlData.IOS_MARKET_PREFIX}id373742138';
         case 'newliiv': // 리브 Next
-          return UrlData.IOS_MARKET_PREFIX + 'id1573528126';
+          return '${UrlData.IOS_MARKET_PREFIX}id1573528126';
         default:
-          return this.url;
+          return _url;
       }
     } else if (Platform.isAndroid) {
-      if (this.package != null) {
+      if (_package != null) {
         // 앱이 설치되어 있지 않아 실행 불가능할 경우 추출된 package 정보를 이용해 플레이스토어 열기
-        return UrlData.ANDROID_MARKET_PREFIX + this.package!;
+        return UrlData.ANDROID_MARKET_PREFIX + _package!;
       }
-      switch (this.appScheme) {
+      switch (_appScheme) {
         case UrlData.ISP:
           return UrlData.ANDROID_MARKET_PREFIX + UrlData.PACKAGE_ISP;
         case UrlData.BANKPAY:
@@ -218,22 +216,24 @@ class IamportUrl {
         case UrlData.NAVER:
           return UrlData.ANDROID_MARKET_PREFIX + UrlData.PACKAGE_NAVER;
         default:
-          return this.url;
+          return _url;
       }
     }
     return null;
   }
 
   Future<bool> launchApp() async {
-    bool opened = false;
-    String appUrl = (await this.getAppUrl())!;
+    bool opened;
+    final appUrl = (await getAppUrl())!;
 
     try {
       opened = await launchUrlString(appUrl);
-    } catch (e) {}
+    } catch (e) {
+      opened = false;
+    }
 
     if (!opened) {
-      opened = await launchUrlString((await this.getMarketUrl())!);
+      opened = await launchUrlString((await getMarketUrl())!);
     }
 
     return opened;
--- lib/model/iamport_validation.dart
+++ lib/model/iamport_validation.dart
@@ -1,77 +1,81 @@
 import 'package:iamport_flutter/model/certification_data.dart';
 import 'package:iamport_flutter/model/payment_data.dart';
 
+// ignore_for_file: avoid_unused_constructor_parameters
 class IamportValidation {
-  bool isValid = true;
-  String? errorMessage;
-
   IamportValidation(String userCode, PaymentData data, Function callback) {
     if (data.payMethod == 'vbank') {
       if (data.vbankDue == null) {
-        isValid = false;
-        errorMessage = '가상계좌 결제시 입금기한(vbankDue)은 필수입력입니다.';
+        _isValid = false;
+        _errorMessage = '가상계좌 결제시 입금기한(vbankDue)은 필수입력입니다.';
         return;
       }
 
       if (data.pg == 'danal_tpay' && data.bizNum == null) {
-        isValid = false;
-        errorMessage = '다날 - 가상계좌 결제시 사업자 등록번호(bizNum)은 필수입력입니다.';
+        _isValid = false;
+        _errorMessage = '다날 - 가상계좌 결제시 사업자 등록번호(bizNum)은 필수입력입니다.';
         return;
       }
     }
 
     if (data.payMethod == 'phone' && data.digital == null) {
-      isValid = false;
-      errorMessage = '휴대폰 소액결제시 실물 컨텐츠 여부(digital)는 필수입력입니다.';
+      _isValid = false;
+      _errorMessage = '휴대폰 소액결제시 실물 컨텐츠 여부(digital)는 필수입력입니다.';
       return;
     }
 
     if (data.pg == 'kcp_billing' && data.customerUid == null) {
-      isValid = false;
-      errorMessage = '정기결제시 구매자 카드정보(customerUid)는 필수입력입니다.';
+      _isValid = false;
+      _errorMessage = '정기결제시 구매자 카드정보(customerUid)는 필수입력입니다.';
       return;
     }
 
     if (data.pg == 'syrup') {
-      isValid = false;
-      errorMessage = '해당 모듈은 해당 PG사를 지원하지 않습니다.';
+      _isValid = false;
+      _errorMessage = '해당 모듈은 해당 PG사를 지원하지 않습니다.';
       return;
     }
 
-    if (data.pg == 'paypal' && data.popup == true) {
-      isValid = false;
-      errorMessage = '해당 모듈에서 페이팔 - 팝업 방식은 지원하지 않습니다.';
+    if (data.pg == 'paypal' && (data.popup ?? false)) {
+      _isValid = false;
+      _errorMessage = '해당 모듈에서 페이팔 - 팝업 방식은 지원하지 않습니다.';
       return;
     }
 
     if ((data.pg == 'naverpay' || data.pg == 'naverco') &&
-        data.naverPopupMode == true) {
-      isValid = false;
-      errorMessage = '해당 모듈에서 네이버페이 - 팝업 방식은 지원하지 않습니다.';
+        (data.naverPopupMode ?? false)) {
+      _isValid = false;
+      _errorMessage = '해당 모듈에서 네이버페이 - 팝업 방식은 지원하지 않습니다.';
       return;
     }
 
-    if (data.popup == true) {
-      isValid = false;
-      errorMessage = '해당 모듈은 팝업 방식을 지원하지 않습니다.';
+    if (data.popup ?? false) {
+      _isValid = false;
+      _errorMessage = '해당 모듈은 팝업 방식을 지원하지 않습니다.';
       return;
     }
   }
 
   IamportValidation.fromCertificationData(
-      String userCode, CertificationData data, Function callback) {
-    if (data.popup == true) {
-      isValid = false;
-      errorMessage = '해당 모듈은 팝업 방식을 지원하지 않습니다.';
+    String userCode,
+    CertificationData data,
+    Function callback,
+  ) {
+    if (data.popup ?? false) {
+      _isValid = false;
+      _errorMessage = '해당 모듈은 팝업 방식을 지원하지 않습니다.';
       return;
     }
   }
 
+  bool _isValid = true;
+  String? _errorMessage;
+
   bool getIsValid() {
-    return isValid;
+    return _isValid;
   }
 
   String? getErrorMessage() {
-    return errorMessage;
+    return _errorMessage;
   }
 }
